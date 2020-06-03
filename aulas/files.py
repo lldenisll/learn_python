fbe0ac74d438f1e780e94e37534ef538d22f8dca
Denis <namorado@TFGcos-MacBook-Pro.local>
2020-06-03 14:38:29 -0300

    manipualcao textos

---
+++ aulas/files.py
@@ -0,0 +1,42 @@
+#arquivo=open('teste.txt', 'w') #criar um arquivo novo
+def escrever_arquivo(texto):
+
+    arquivo=open('teste.txt', 'w') #cria o arquivo
+    arquivo.write(texto)
+    arquivo.close()
+
+
+def atualizar_arquivo(nome_arquivo, texto):
+    arquivo = open(nome_arquivo, 'a')  # edita o arquivo existente
+    arquivo.write(texto)
+    arquivo.close()
+def ler_arquivo(nome_arquivo):
+    arquivo=open(nome_arquivo, 'r') #formato r de read
+    texto=arquivo.read()
+    print(texto)
+def cad_usuario(nome_arquivo):
+    arquivo=open(nome_arquivo, 'r')
+    usuario=arquivo.read()
+    usuario=usuario.split('\n')
+    for x in usuario:
+        lista_usuarios=x.split(",")
+        #print(lista_usuarios) imprime a lsita completa de usuarios
+        us=lista_usuarios[0]
+        print(us) #imprime apenas o usuário
+def copia_arquivo(nome_arquivo):
+    import shutil
+    shutil.copy(teste.txt,"/Users/namorado")
+
+if __name__ == '__main__':
+    #escrever_arquivo("Primeira linha")
+
+    usuario=input("Nome do usuário: ")
+    tipo=input("Escreva o tipo de usuário: ")
+    cadastro=("\n"+usuario + "," + tipo)
+    atualizar_arquivo("teste.txt",cadastro)
+    cad_usuario('teste.txt')
+    #item=input("Digite o próximo item: ")
+    #lista_mercado=item+"\n"
+    #atualizar_arquivo("teste.txt", lista_mercado)
+    #ler_arquivo('teste.txt')
+
--- aulas/lambda.py
+++ aulas/lambda.py
@@ -6,3 +6,19 @@ print(contador_letras(lista))
 soma=lambda a,b: a+b #lambda chama a função, depois vem os parametros, e a execução
 
 print(soma(4,2))
+
+calculadora={  #dicionário
+    'soma': lambda a,b: a+b,
+    'subtração': lambda a,b: a-b,
+    'multiplicacao': lambda a,b: a*b,
+}
+
+soma=calculadora['soma']
+print(soma(10,4))
+
+valida_numero = {
+    'par': lambda a: True if a % 2 == 0 else False,
+    'impar': lambda b: True if b % 2 == 0 else False
+}
+resultado = valida_numero['par'](10)
+print(resultado)
